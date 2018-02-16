945f5b5ad6fbc3923f40d7cfcfe50bdd7958dc16
Divesh Otwani <divesh.otwani@gmail.com>
2018-02-15 20:11:10 -0500

    almost done

--- simple-examples/README.md
+++ simple-examples/README.md
@@ -7,10 +7,10 @@ and clear enough to give you an idea of how things work.
 To learn dash and these tools, you should go through 
 the materials in this order.
 
-_Note: You don't need to do all of these. Do the first three at least_
+_Note: You don't need to do all of these. Do the first three or four at least_
 
 
-* Read the *Background* section below
+* Read the ** section below
 * hello.py
   * A basic hello world app.
 * palindrome.py
@@ -28,19 +28,101 @@ and browse the rest of the documentation [here](https://plot.ly/dash/).
 
 
 
+
+> **Read this part below here !!!**
+
+
 # Background on what we're doing
 
 
-## What's a web server and why do I care?
+## What are web servers?
+
+If you're unfamiliar, a web server is just an infinite while loop
+that responds to messages it gets sent over the internet.
+
+### Intuition
+
+So, when you visit gmail, facebook, reddit, twitter, and click on things,
+type things in, (or even hit enter to the url),
+messages are sent to that program, called a web server, on a computer 
+far far away and that program responds responds by changing the web page
+that's displayed to you.
 
 
-## What's html?
+### More than Intuition: A Clear Description
+
+* Your web browser displays websites with all their buttons, text fields,
+  colors and stuff using html files. 
+  Html files are like instructions on how to 
+  display a nice looking website.
+  * Read [this](http://www.simplehtmlguide.com/whatishtml.php) and 
+    test the example on your own computer.
+  * This is an example of a static web site.
+    You don't need to run a while loop because it does 
+    just one task.
+  * Also, skim some of [https://www.w3schools.com/html/default.asp](https://www.w3schools.com/html/default.asp)
+* When you visit a website, your web browser sends a message to the program 
+  called the web server (which runs on a computer far away) to 
+  send you the html file to display the web page. This message
+  is sent over the internet.
+  * This is what loads the first page.
+  * This also happens anytime you click on links
+* You can think of a web page as just some really long string
+  (that's basically what files are). This string is sent over the internet
+  from the web server to your computer and then your web browser 
+  displays it really nicely.
+* Then, sometimes you click buttons and enter input.
+  Some of this input can be responded to with tiny programs 
+  inside your html, that string, written in a (terrible) programming langugage
+  called javascript.
+  But, responding to some of your input requires the program that sent 
+  you the website to do something. 
+  (Remember, the program that sent you the website is called a web server.)
+  For example, clicking the sent mail button
+  in gmail. In these cases your web browser sends a  message over the internet
+  to the web server.
+  That program, the web server, understands your message and sends you a new 
+  html file for your web browser to display.
+* This summarizes the basics of websites and internet use.
+* Other notes
+  * Sometimes the web server saves some information you send it
+    on the computer it runs on (far, far away).
+  * The messages you send are broken into categories, 
+    'get' messages, 'post' messages are two.
+  * With secure sites, rsa encrytpion is used to 
+    hide the messages sent to the web server.
+
+
+#### What do I need to know to make web servers?
+
+ * You need to know some background
+  * You need to know how html is displayed in your web browser
+  * If I asked you to write a website that just 
+    displays some images and some essays I wrote for
+    some classes with a nice background and stuff, 
+    you should be able to.
+ * You need to understand tools like dash that help you build html
+  * You do NOT need to know exactly what html is built.
+    You just need to have a clear idea of what that html does.
 
 
 ## How does dash work to build a webserver?
 
+ * See the examples
 
 ## Why dash and not some other tool?
+
+ * Other tools are not in python and are 
+   really complicated.
+ * (If we did this in haskell, it would 
+   be way simpler!)
+  
+## How do I put my web server "online"?
+
+This is called deploying a website.
+You can read about it [here](https://www.codeschool.com/beginners-guide-to-web-development/deploying-your-first-website), but 
+we can just take care of that for you if you want.
+(Obviously, we won't take any credit for your work!)
 
 
 
--- simple-examples/palindrome.py
+++ simple-examples/palindrome.py
@@ -6,15 +6,32 @@ from dash.dependencies import Input, Output, State
 
 """
 Just import the stuff above.
+You will need all of it, usually in each file.
 """
 
 app = dash.Dash()
 
 """
 dash.Dash() is your actual web server as an object.
-It's an instance of the class Dash().
+It's an instance of the class dash.Dash().
 
-All you do is
+Hopefully you are familiar enough with imports to understand
+how you can refer to things you define in imported files;
+I can get dcc.someFunction for example.
+
+------------------------------------------------------
+Building a web server.
+
+------------------------------------------------------
+Right now, we are building a web server
+that displays one page, and on that page there is box to type
+a string and a button that says "check if palindrome"
+and once we click the button, some text is printed
+telling us if we have a palindrome.
+
+
+
+For making a single web page, do you this:
 
 1) Build html elements and nest all of them in some large html element.
 2) Assign the large html element to app.layout
@@ -22,32 +39,128 @@ All you do is
 
 and it has to be done in that exact order.
 
-Of course, there's a bunch of questions here:
+The background from the README.md file that is in the same folder
+as this file covered most of what you need to know.
 
-0) What is a webserver? What am I making anyway?
-1) What is html? What are html elements and how do I make one in python?
-2) How do I nest html elements inside each other?
-3) What are callback functions? How do I write them?
-What do they have to do with what I'm making?
+All you need now, is to know
+ 1) How do I make html elements in here?
+ 2) What are callback functions?
 
-TODO: I'll finish the documenation tomorrow.
-Add a button and call back then ... finish the code.
+We answer these below.
 
 """
 
+
+
+##############################################################
+#                Making HTML Elements
+##############################################################
+
 title = html.H1(children=["A Simple Palindrome Example"])
 
-textInputBox = dcc.Input(id='inputbox', value='', type='text')
-textResponse = html.Div(id='response')
+# From reading the links in the readme, you should know about
+# the html H1 to H6 tags: https://www.w3schools.com/tags/tag_hn.asp
+# This represents that tag
+# The first argument is named "children" for html.Anything
+# (See here http://www.diveintopython.net/power_of_introspection/optional_arguments.html)
+# The children is a list of other html elements
+# You can use strings for html elements when it's simple enough
 
-topHtmlElement = html.Div([title, textInputBox, textResponse])
+stringInputBox = dcc.Input(id='inputbox-id', value='', type='text')
+
+# Input boxes are bit more complicated,
+# You make an object that is an instance of the class dcc.Input
+# Then, you specify a string for an unique id,
+# An initial value to display
+# and that the type of input is text like shown
+
+checkIfPalButton = html.Button(children='Check If Palindrome', id='button-id')
+# Buttons are similar
+
+displayIfPalindrome = html.Div(children=[], id='response-id')
+# This is div html tag (look it up on w3school.org)
+# It has no children. I could have written
+# html.Div(id='response-id') and that would be the same
+# The default value of children is []
+# We will insert html elements into the list children when
+# the button is clicked.
+
+listOfHtmlElements = [title, stringInputBox,checkIfPalButton,displayIfPalindrome]
+
+topHtmlElement = html.Div(children=listOfHtmlElements)
+
+##############################################################
+##############################################################
+
+
+# Now, you say that the web page you want to display
+# just shows this one html element.
 app.layout = topHtmlElement
 
-@app.callback(Output('response', 'children'), [Input('inputbox', 'value')], [])
-def doSomethingWithTextInput(textInput):
-    youEntered = "You entered {}".format(textInput)
-    return youEntered
 
 
+
+
+
+
+
+##############################################################
+#             Making Callback Functions
+##############################################################
+
+# You know this function:
+def isPal(s):
+    if len(s) < 3:
+        return True
+    else:
+        first = s[0]
+        last = s[-1]
+        return (first == last) and isPal(s[1:-1])
+
+
+@app.callback(
+    Output('response-id', 'children'),
+    [Input('button-id', 'n_clicks')],
+    [State('inputbox-id', 'value')]
+    )
+def doSomethingWithTextInput(numClicksOfButton, textInput):
+    isPalMessage = "\"{}\" is in fact a palindome!".format(textInput)
+    isNotPalMessage = "\"{}\" is NOT a palindome!".format(textInput)
+    retMsg = isPalMessage if isPal(textInput) else isNotPalMessage
+    return retMsg
+
+"""
+Alright, how do callbacks work?
+
+at the top you write
+@app.callback(Output(...), listOfInput, listOfState)
+
+The first arguement is an instance of Output.
+Your provide two arguments, the first is the id of
+the html element inside of which you will put
+the html element that this function creates.
+
+The second argument is a list of instances of Input.
+The first arguement is the id.
+The second is the 
+
+
+
+"""
+
+
+
+##############################################################
+##############################################################
+
+
+
+"""
+Always have these few lines at the bottom of your code.
+It just runs your web server
+when you do
+ $ python palindrome.py
+on the command line
+"""
 if __name__ == '__main__':
     app.run_server(debug=True)
